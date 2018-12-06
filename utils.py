c81fcbdb4dafd37e049e762bda8e7ba2d9afdded
Samarth Brahmbhatt <samarth.robo@gmail.com>
2018-12-05 22:11:38 -0500

    modified plyfile to work with memory buffers

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from plyfile import PlyData
 import matplotlib.pyplot as plt
 import numpy as np
+import urllib.request
+import io
+import plotly.graph_objs as go
 
 def texture_proc(tex, sigmoid_k=10.0, max_frac=0.75):
     idx = tex > 0
@@ -12,16 +15,23 @@ def texture_proc(tex, sigmoid_k=10.0, max_frac=0.75):
     return colors
 
 def read_ply(filename, sigmoid_k=10.0, max_frac=0.75):
-    with open(filename, 'rb') as f:
-        plydata = PlyData.read(f)
-    x = np.asarray(plydata['vertex'].data['x'])
-    y = np.asarray(plydata['vertex'].data['y'])
-    z = np.asarray(plydata['vertex'].data['z'])
-    tex = np.asarray(plydata['vertex'].data['red'])
-    
-    vertices = np.vstack((x, y, z)).T
-    faces = np.vstack(plydata['face'].data['vertex_indices'])
-    colors = texture_proc(tex/255.0, sigmoid_k=sigmoid_k,
-                          max_frac=max_frac)
-    
-    return vertices, faces, colors
+  with urllib.request.urlopen(filename) as r:
+  # with open(filename, 'rb') as r:
+    f = io.BufferedReader(io.BytesIO(r.read()))
+  plydata = PlyData.read(f)
+  x = np.asarray(plydata['vertex'].data['x'])
+  y = np.asarray(plydata['vertex'].data['y'])
+  z = np.asarray(plydata['vertex'].data['z'])
+  tex = np.asarray(plydata['vertex'].data['red'])
+  
+  vertices = np.vstack((x, y, z)).T
+  faces = np.vstack(plydata['face'].data['vertex_indices'])
+  colors = texture_proc(tex/255.0, sigmoid_k=sigmoid_k,
+                        max_frac=max_frac)
+  
+  mesh = go.Mesh3d(x=vertices[:, 0], y=vertices[:, 1], z=vertices[:, 2],
+               i=faces[:, 0], j=faces[:, 1], k=faces[:, 2])
+  mesh['flatshading'] = False
+  mesh['vertexcolor'] = colors
+  
+  return mesh
